d1f6c480ade7b9b388c66df218ae82854206195c
Leonardo2745 <leonardoaugustodiascardosoc@gmail.com>
2024-03-06 22:13:51 -0300

    AULA DE ONTEM

--- app/script.js
+++ app/script.js
@@ -1,41 +1,49 @@
-let data = []
+let data = [];
 
 const submitComment = (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const author = inputAuthor.value;
-    const comment = inputComment.value;
+  const author = inputAuthor.value;
+  const comment = inputComment.value;
 
-    data.push({ author: author, comment: comment })
-    console.log(data)
+  data.push({ author: author, comment: comment });
+  console.log(data);
 
-    loadComment()
-}
+  loadComment();
+};
 
-const formComentario = document.getElementById('formComment')
-formComentario.addEventListener("submit", submitComment)
+const formComentario = document.getElementById("formComment");
+formComentario.addEventListener("submit", submitComment);
 
 const loadComment = () => {
-    // Dados carregados da API
-    if (data) {
-        displayComment();
-    }
-}
+  // Dados carregados da API
+  if (data) {
+    displayComment();
+  }
+};
 
 const displayComment = () => {
-    const body = document.getElementsByTagName('body')[0];
-
-    data.forEach(elemento => {
-        const divDisplay = document.createElement('div');
-        divDisplay.className = 'comentarios'
-        divDisplay.innerHTML = `
-        <strong>
-        <span class="badge text-bg-secondary">${elemento.author}</span>
-        </strong>
-        <p>${elemento.comment}</p>
-        `
-        body.appendChild(divDisplay);
-    })
-
-
-}
+  const divFeed = document.getElementById("comment-feed");
+divFeed.innerHTML = ``;
+  data.forEach((item) => {
+    const divDisplay = document.createElement("div");
+    divDisplay.className = "comentarios";
+    divDisplay.innerHTML = ` 
+        <div class="d-flex text-body-secondary pt-3">
+        <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32"
+            xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32"
+            preserveAspectRatio="xMidYMid slice" focusable="false">
+            <title>Comment</title>
+            <rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff"
+                dy=".3em">32x32</text>
+        </svg>
+        <div>
+        <p class="pb-3 mb-0 small lh-sm border-bottom" id="showComments">
+            <strong class="d-block text-gray-dark">@${item.author}</strong>
+            ${item.comment}
+        </p>
+    </div>
+        `;
+    divFeed.appendChild(divDisplay);
+  });
+};
